d58f09d4a26dd9bfd4c7b453aed3fa82dfd24427
steven c <stevenc.github@gmail.com>
2020-07-16 15:32:18 -0400

    Add cheeses app

--- everycheez/config/settings/base.py
+++ everycheez/config/settings/base.py
@@ -84,7 +84,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "everycheez.users.apps.UsersConfig",
-    # Your stuff: custom apps go here
+    "everycheez.cheeses.apps.CheesesConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
